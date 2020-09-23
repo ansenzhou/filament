449bd0b4065e7f9a2a4d584acd334c4e408108c9
Pixelflinger <pixelflinger@gmail.com>
2020-09-23 01:14:34 -0700

    add support for highp and naming/formating

--- filament/src/materials/ssao/dominantLightShadowing.fs
+++ filament/src/materials/ssao/dominantLightShadowing.fs
@@ -1,19 +1,19 @@
 /*
- * Dominant Light Shadowing
- * see: "Lighting Technology of The Last of Us Part II" by Hawar Doghramachi, Naughty Dog, LLC
+ * Largely based on "Dominant Light Shadowing"
+ * "Lighting Technology of The Last of Us Part II" by Hawar Doghramachi, Naughty Dog, LLC
  */
 
 #include "ssaoUtils.fs"
 
 struct ConeTraceSetup {
     // runtime parameters
-    vec2 startPosSS;
-    vec3 startPosVS;
-    vec3 normalVS;
-    mat4 viewToScreenMat;
+    highp vec2 ssStartPos;
+    highp vec3 vsStartPos;
+    vec3 vsNormal;
+    highp mat4 viewToScreenMat;
     vec2 jitterOffset;          // (x = direction offset, y = step offset)
-    float depthParams;
-    vec3 coneTraceDirVS;
+    highp float depthParams;
+    vec3 vsConeDirection;
 
     // artistic/quality parameters
     vec4 coneTraceParams;       // (x = slope, y = start trace distance, z = inverse max contact distance, w = intensity)
@@ -23,97 +23,88 @@ struct ConeTraceSetup {
     uint sampleCount;
 };
 
-float GetInvW(vec3 startPosVS, mat4 viewToScreenMat) {
-    return 1.0 / (viewToScreenMat * vec4(startPosVS, 1.0)).w;
-}
-
-vec2 GetExtScreenTC(vec3 endPosVS, mat4 viewToScreenMat, out float endInvW) {
-    vec4 p = viewToScreenMat * vec4(endPosVS, 1.0);
-    endInvW = 1.0 / p.w;
-    return p.xy * endInvW;
-}
-
 float coneTraceOcclusion(in ConeTraceSetup setup, const sampler2D depthTexture) {
     // skip fragments that are back-facing trace direction
     // (avoid overshadowing of translucent surfaces)
-    float nDotD = dot(setup.normalVS, setup.coneTraceDirVS);
-    if (nDotD < 0.0f) {
-        return 0.0f;
+    float NoL = dot(setup.vsNormal, setup.vsConeDirection);
+    if (NoL < 0.0) {
+        return 0.0;
     }
 
     // start position of cone trace
-    vec2 startPosSS = setup.startPosSS;
-    vec3 startPosVS = setup.startPosVS;
-    float startInvW = GetInvW(startPosVS, setup.viewToScreenMat);
+    highp vec2 ssStartPos = setup.ssStartPos;
+    highp vec3 vsStartPos = setup.vsStartPos;
+    highp float ssStartInvW = 1.0 / (setup.viewToScreenMat * vec4(vsStartPos, 1.0)).w;
 
     // end position of cone trace
-    vec3 endPosVS = setup.coneTraceDirVS * 0.1f + startPosVS;
-    float endInvW;
-    vec2 endPosSS = GetExtScreenTC(endPosVS, setup.viewToScreenMat, endInvW);
+    highp vec3 vsEndPos = setup.vsConeDirection * 0.1 + vsStartPos;
+    highp vec4 ssEndPos = setup.viewToScreenMat * vec4(vsEndPos, 1.0);
+    highp float ssEndInvW = 1.0 / ssEndPos.w;
+    ssEndPos.xy *= ssEndInvW;
 
     // cone trace direction in screen-space
-    vec2 coneDir = endPosSS - startPosSS;
-    float invConeDirLength = 1.0f / length(coneDir);
-    coneDir *= invConeDirLength;
+    float ssConeLength = length(ssEndPos.xy - ssStartPos);
+    float ssInvConeLength = 1.0 / ssConeLength;
+    vec2 ssConeDirection = (ssEndPos.xy - ssStartPos) * ssInvConeLength;
 
     // direction perpendicular to cone trace direction
-    vec2 perpConeDir = float2(coneDir.y, -coneDir.x);
+    vec2 perpConeDir = vec2(ssConeDirection.y, -ssConeDirection.x);
 
     // avoid self-occlusion and reduce banding artifacts by normal variation
-    vec3 viewVecVS = normalize(startPosVS);
-    float minTraceDistance = (1.0f - abs(dot(setup.normalVS, viewVecVS))) * 0.005f;
+    vec3 vsViewVector = normalize(vsStartPos);
+    float minTraceDistance = (1.0 - abs(dot(setup.vsNormal, vsViewVector))) * 0.005;
 
     // init trace distance and sample radius
-    float invLinearDepth = 1.0f / -setup.startPosVS.z;
-    float traceDistance = max(setup.coneTraceParams.y, minTraceDistance) * invLinearDepth;
-    float sampleRadiusSS = setup.coneTraceParams.x * traceDistance;
-    float endRadiusSS = setup.coneTraceParams.x / invConeDirLength;
-    float endRadiusVS = endRadiusSS * setup.invZoom * invLinearDepth / endInvW;
+    highp float invLinearDepth = 1.0 / -setup.vsStartPos.z;
+    float ssTraceDistance = max(setup.coneTraceParams.y, minTraceDistance) * invLinearDepth;
+
+    float ssSampleRadius = setup.coneTraceParams.x * ssTraceDistance;
+    float ssEndRadius    = setup.coneTraceParams.x * ssConeLength;
+    float vsEndRadius    = ssEndRadius * setup.invZoom * invLinearDepth * ssEndPos.w;
 
     // calculate depth bias
-    float depthBias = saturate(1.0f - nDotD) * setup.slopeScaledDepthBias + setup.depthBias;
+    float vsDepthBias = saturate(1.0 - NoL) * setup.slopeScaledDepthBias + setup.depthBias;
 
-    float occlusion = 0.0f;
-    for (uint i = 0; i < setup.sampleCount; i++) {
+    float occlusion = 0.0;
+    for (uint i = 0u; i < setup.sampleCount; i++) {
         // step along cone in screen space
-        float newSampleRadiusSS = sampleRadiusSS * (sampleRadiusSS + traceDistance) / (traceDistance - sampleRadiusSS);
-        float stepDistance = sampleRadiusSS + newSampleRadiusSS;
-        sampleRadiusSS = newSampleRadiusSS;
+        float ssNewSampleRadius = ssSampleRadius * (ssSampleRadius + ssTraceDistance) / (ssTraceDistance - ssSampleRadius);
+        float ssStepDistance    = ssSampleRadius + ssNewSampleRadius;
+        ssSampleRadius = ssNewSampleRadius;
 
         // apply jitter offset
-        float jitterDistance = stepDistance * setup.jitterOffset.y;
-        float jitteredTracedDistance = traceDistance + jitterDistance;
-        float jitteredSampleRadius = setup.jitterOffset.x * sampleRadiusSS * jitterDistance / stepDistance;
+        float ssJitterDistance = ssStepDistance * setup.jitterOffset.y;
+        float ssJitteredTracedDistance = ssTraceDistance + ssJitterDistance;
+        float ssJitteredSampleRadius = setup.jitterOffset.x * ssSampleRadius * ssJitterDistance / ssStepDistance;
 
         // sample depth buffer
-        vec2 samplePosSS = perpConeDir * jitteredSampleRadius + coneDir * jitteredTracedDistance + startPosSS;
-        float bufferDepthValue = -sampleDepthLinear(depthTexture, samplePosSS, 0.0, setup.depthParams);
+        highp vec2 ssSamplePos = perpConeDir * ssJitteredSampleRadius + ssConeDirection * ssJitteredTracedDistance + ssStartPos;
+        highp float vsLinearDepth = -sampleDepthLinear(depthTexture, ssSamplePos, 0.0, setup.depthParams);
 
         // calculate depth of cone center
-        float ratio = jitteredTracedDistance * invConeDirLength;
-        float lerpedW = 1.0f / mix(startInvW, endInvW, ratio);
-        float sampleDepth = lerpedW;
+        float ratio = ssJitteredTracedDistance * ssInvConeLength;
+        highp float vsLinearSampleDepth = 1.0 / mix(ssStartInvW, ssEndInvW, ratio);
 
         // calculate depth range of cone slice
-        float sampleRadiusVS = endRadiusVS * ratio * lerpedW;
-        float jitteredSampleRadiusVS = sampleRadiusVS * setup.jitterOffset.x;
-        float sliceHalfRangeVS = sqrt(sampleRadiusVS * sampleRadiusVS + jitteredSampleRadiusVS * jitteredSampleRadiusVS);
-        float sampleDepthMin = sampleDepth - sliceHalfRangeVS;
-        float sampleDepthMax = sampleDepth + sliceHalfRangeVS;
+        float vsSampleRadius = vsEndRadius * ratio * vsLinearSampleDepth;
+        float vsJitteredSampleRadius = vsSampleRadius * setup.jitterOffset.x;
+        float vsSliceHalfRange = sqrt(vsSampleRadius * vsSampleRadius + vsJitteredSampleRadius * vsJitteredSampleRadius);
+        float vsSampleDepthMin = vsLinearSampleDepth - vsSliceHalfRange;
+        float vsSampleDepthMax = vsLinearSampleDepth + vsSliceHalfRange;
 
         // calculate overlap of depth buffer height-field with trace cone
-        float depthDifference = sampleDepthMax - bufferDepthValue;
-        float overlap = saturate((depthDifference - depthBias) / (sampleDepthMax - sampleDepthMin));
+        float vsDepthDifference = vsSampleDepthMax - vsLinearDepth;
+        float overlap = saturate((vsDepthDifference - vsDepthBias) / (vsSampleDepthMax - vsSampleDepthMin));
 
         // attenuate by distance to avoid false occlusion
-        float attenuation = saturate(1.0f - (depthDifference * setup.coneTraceParams.z));
+        float attenuation = saturate(1.0 - (vsDepthDifference * setup.coneTraceParams.z));
         occlusion = max(occlusion, overlap * attenuation);
 
-        if (occlusion >= 1.0f) {
+        if (occlusion >= 1.0) {
             break;
         }
 
-        traceDistance += stepDistance;
+        ssTraceDistance += ssStepDistance;
     }
 
     return occlusion * setup.coneTraceParams.w;
